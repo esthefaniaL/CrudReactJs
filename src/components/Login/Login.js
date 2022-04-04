2c475f294146536d75c090844f7ac379cd8c7a65
Esthefania Lemus <esthefanialemus@gmail.com>
2022-04-04 01:06:39 -0500

    modificacion login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme) => ({
     },
     avatar: {
       margin: theme.spacing(1),
-      backgroundColor: theme.palette.secondary.main,
+      backgroundColor: '#a5dc86',
     },
     form: {
       width: '100%', // Fix IE 11 issue.
